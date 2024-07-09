c6f689140d6b55ed6cda30c15589e953b6b5ec10
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2024-07-09 15:44:56 +0800

    chore(deps): provide chromedriver 116.0.5845.187 for compatibility with specific projects
    This formula allows users to easily install chromedriver version 116.0.5845.187, which may be required for older projects or for testing against specific Chrome releases.

---
+++ chromedriver-116.0.5845.187.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Chromedriver11605845187 < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/Homebrew/homebrew-cask/blob/990ddc90a35e8c1ad3dd75788064ab6753e4afbd/Casks/c/chromedriver.rb"
+  version "116.0.5845.187"
+  sha256 "ca8452c318036bf82f7cae8ee86d849c2fa1905a51c1cc9d8d0f3295f5fa2ee4"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test chromedriver-116.0.5845.187`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
